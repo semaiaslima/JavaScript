9be45a4fd4ed925f6ea40581d513b55d5b436434
Semaías Lima <semaias_oliveira@hotmail.com>
2023-10-01 11:51:11 -0300

    Aprendendo sobre objetos

---
+++ Objetos/aula.js
@@ -0,0 +1,8 @@
+const sema = {
+    nome: 'Semaias de Oliveira Lima',
+    idade: 23
+};
+
+console.log(sema.nome);
+console.log(sema.idade);
+console.log(sema)
---
+++ Objetos/aula2.js
@@ -0,0 +1,8 @@
+const sema = {
+    nome: 'Semaias de Oliveira Lima',
+    idade: 23
+};
+
+sema.altura = 1.72;
+
+console.log)sema
