8c4339237060b852f834e2bd0c69c61893c00c75
Semaías Lima <semaias_oliveira@hotmail.com>
2023-10-08 19:18:17 -0300

    Arrays

---
+++ Array/aula1.js
@@ -0,0 +1,6 @@
+
+
+const alunos = ['João', 'Vitor', 'Marina'];
+
+console.log(alunos);
+console.log(alunos[0]);
---
+++ Array/aula2.js
@@ -0,0 +1,9 @@
+
+
+const alunos = ['Sema', 'Renan', 'Yasmin'];
+
+alunos.push('Marina');
+
+alunos[4] = 'Nathalia';
+
+console.log(alunos);
